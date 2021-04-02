0599f096cb876a6cacd112f17d00d6b7668d18bc
DaSpikeyBoss <daspikeybos@tutanota.com>
2021-04-02 18:45:44 +0900

    Hello World Multiplayer Edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement() {
+        if(isLocalPlayer) {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horizontal * 0.1f, vertical * 0.1f);
+            transform.position += movement;
+        }
+    }
+
+    void Update() {
+        HandleMovement();
+    }
+}
